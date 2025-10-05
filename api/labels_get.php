3fa3baf03e696eea4de9a9111ea61e7167aa88b9
horizonsexplorer <a-kimo-ck@hotmail.com>
2025-10-05 14:02:14 +0300

    Delete api directory

--- api/labels_get.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Developed by Abdulwahab Almusailem
-header('Content-Type: application/json');
-$world = $_GET['world'] ?? 'earth';
-$file = __DIR__ . '/../data/labels-' . basename($world) . '.json';
-if (!file_exists($file)) { echo '[]'; exit; }
-echo file_get_contents($file);
